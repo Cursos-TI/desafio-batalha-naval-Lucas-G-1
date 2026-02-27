39cde54106b984e335461cd484f6b6fb5f4ca0e3
Lucas-G-1 <122480830+Lucas-G-1@users.noreply.github.com>
2026-02-27 22:54:42 +0000

    Criação do codigo Batalha Naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -9,6 +9,64 @@ int main() {
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    //======================= CONFIGURAÇÕES INICIAIS ====================================
+    // Tamanho fixo do tabuleiro (10x10)
+    int TamanhoTabuleiro = 10;
+
+    //tamanho fixo dos navios (ambos com 3 posições)
+    int TamanhoNavio = 3;
+
+    //matriz do tabuleiro: 0 = agua, 3 = parte do navio
+    int tabuleiro[10][10] = {0}; //iniciar tudo com 0 automaticamente
+
+    //======================= POSICIONAMENTO DO NAVIO HORIZONTAL ========================
+    //Navio horizontal: ocupa 3 casas na mesma linha
+    int linhaHorizontal = 2;
+    int colunaHorizontal = 4;
+
+    //posiciona o navio horizontal (preenche com 3)
+    for (int i = 0; i < TamanhoNavio; i++){
+        tabuleiro[linhaHorizontal][colunaHorizontal + i] = 3;
+    }
+
+    //======================== POSICIONAMENTO DO NAVIO VERTICAL ========================
+    // Navio vertical: ocupa 3 casas na mesma coluna
+    int ColunaVertical = 7;
+    int LinhaVertical = 5;
+
+    //Posiciona o navio vertical (Preeche com 3)
+    for (int j = 0; j < TamanhoNavio; j++){
+        tabuleiro[LinhaVertical + j][ColunaVertical] = 3;
+    }
+
+    //====================== EXIBIÇAO DO TABULEIRO ====================
+    //Loops aninhados: externo = linhas, interno = coluna
+    printf("\n=== Tabuleiro de Batalha Naval (10x10) ===\n");
+    printf(" ");// Espaço para alinhar os numeros das colunas
+
+    // Cabeçalho das colunas (0 a 9)
+    for (int c = 0; c < TamanhoTabuleiro; c++) {
+        printf("%2d", c);
+    }
+    printf("\n");
+
+    //imprime cada linha
+    for (int linha = 0; linha < TamanhoTabuleiro; linha++){
+        //numero da linha no lado esquerdo
+        printf("%2d ", linha);
+        for (int coluna = 0; coluna < TamanhoTabuleiro; coluna++)
+        {
+            //numero 0 ou 3 com espaço para vizualização clara
+            printf("%2d ", linha);
+        }
+        printf("\n");
+    }
+
+    printf("\nLegenda:\n");
+    printf("0 = Água\n");
+    printf("3 = Parte do navio\n");
+    printf("Navio horizontal: linha %d, coluna %d a %d\n", linhaHorizontal, colunaHorizontal, colunaHorizontal + TamanhoNavio - 1);
+    printf("Navio Vertical: coluna %d, linha %d a %d\n", ColunaVertical, LinhaVertical, LinhaVertical + TamanhoNavio - 1);
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
